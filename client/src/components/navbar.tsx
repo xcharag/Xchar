3b66dac6e8a8cb913e86fa63f1be9475186762e0
Oscar Aguilar Soliz <oscaraguilarfut@gmail.com>
2023-12-30 00:59:51 -0400

    First Changes for UI

---
+++ client/src/components/navbar.tsx
@@ -0,0 +1,18 @@
+import {  Link } from "react-router-dom";
+function Navbar(){
+  return <div className="navbar">
+    <li>
+      <Link to="/">Dogs</Link>
+    </li>
+    <li>
+      <Link to="/cats">Cats</Link>
+    </li>
+    <li>
+      <Link to="/sheeps">Sheeps</Link>
+    </li>
+    <li>
+      <Link to="/goats">Goats</Link>
+    </li>
+  </div>
+}
+export default Navbar;
---
+++ client/src/pages/tournaments.tsx
@@ -0,0 +1,11 @@
+const torneos = () =>{
+    return (
+      <div>
+        <h3>Torneos</h3>
+        <div>
+            <h5>Hola Mundo!!!</h5>
+        </div>
+      </div>
+    );
+  }
+export default torneos;
